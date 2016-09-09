d9604b3c4a0b146cfb5ea968bbe5bc378faadd63
Leonardo Castaño <lcastano@ucmq.com>
2016-09-09 14:38:28 -0300

    Update MedicalServiceRepository Class

--- UCMQ-Prestaciones-API/src/main/java/com/unidadcoronaria/prestaciones/repository/MedicalServiceRepository.java
+++ UCMQ-Prestaciones-API/src/main/java/com/unidadcoronaria/prestaciones/repository/MedicalServiceRepository.java
@@ -1,12 +1,16 @@
 package com.unidadcoronaria.prestaciones.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.unidadcoronaria.prestaciones.domain.MedicalService;
 
 public interface MedicalServiceRepository extends CrudRepository<MedicalService, Long> {
 
-    MedicalService findById(Long id);
+    //MedicalService findByMedicalServiceId(Integer id);
+    
+    public List<MedicalService> findByMedicalServiceId(Integer id);
     
     
 }
--- UCMQ-Prestaciones-API/src/main/java/com/unidadcoronaria/prestaciones/service/MedicalServiceServiceImpl.java
+++ UCMQ-Prestaciones-API/src/main/java/com/unidadcoronaria/prestaciones/service/MedicalServiceServiceImpl.java
@@ -4,10 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.unidadcoronaria.prestaciones.domain.MedicalService;
+import com.unidadcoronaria.prestaciones.repository.MedicalServiceRepository;
 
 /**
  * @author AGUSTIN.BALA
@@ -19,10 +21,14 @@ import com.unidadcoronaria.prestaciones.domain.MedicalService;
 public class MedicalServiceServiceImpl implements MedicalServiceService {
 
 	final static Logger logger = Logger.getLogger(MedicalServiceServiceImpl.class);
+	
+	@Autowired
+    private MedicalServiceRepository medicalServiceRepository;
 
 	@Override
 	public List<MedicalService> getMedicalServiceList() {
-		return Arrays.asList(new MedicalService(1l));
+		//return Arrays.asList();
+		return medicalServiceRepository.findByMedicalServiceId(7170056);
 	}	
 	
 
